d29f70e685be8996c6e18f84584e55cf8ce03953
BuildTools <unconfigured@null.spigotmc.org>
2024-02-13 15:33:08 +0100

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tpApp';
+  helloText = "Hello World";
+  mathCoursePic : any = {
+    imageUrl: 'assets/images/maths.jpg',
+    imageWidth : 200
+  };
+
+  imageUrls = [
+    "assets/images/maths.jpg",
+    "assets/images/sport.jpg",
+    "assets/images/chimie.jpg",
+    "assets/images/histoire.jpg",
+    "assets/images/info.jpg",
+    "assets/images/philo.jpg",
+    "assets/images/physique.jpg"
+  ];
+  changeImage(): void {
+    let num = Math.floor((Math.random() * 7)+1)
+    this.mathCoursePic.imageUrl = this.imageUrls[num-1];
+  }
+  writtenWord: string = 'Hello';
+  date = new Date();
+  isVisible: boolean = true;
+  toggleVisibility(): void {
+    this.isVisible = !this.isVisible;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +13,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
